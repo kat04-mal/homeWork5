85b19dc74d786e98a1eeef7f53f1dc40d440c40a
kat04-mal <k-04maleeva@yandex.ru>
2025-09-18 20:24:58 +0300

    Исправление 3 задачи

--- src/Main.java
+++ src/Main.java
@@ -33,16 +33,14 @@ public class Main {
         //Задача 3
         int year = 2020;
         System.out.println("\nЗадача 3");
-        if (year >= 1584){
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
-                System.out.println(year + " год является високосным");
-            }
-            else {
-                System.out.println(year + " год не является високосным");
-            }
+        if (year < 1584){
+            System.out.println("Високосный год был введен в 1584 году");
+        }
+        else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println(year + " год является високосным");
         }
         else {
-            System.out.println("Високосный год был введен в 1584 году");
+            System.out.println(year + " год не является високосным");
         }
 
 
